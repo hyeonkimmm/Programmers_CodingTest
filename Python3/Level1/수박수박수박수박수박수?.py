0b8ee1aec918b662a64532e60a3c1d63bcfc90c6
hyeonkimmm <43769123+hyeonkimmm@users.noreply.github.com>
2020-06-15 18:48:09 +0900

    slicing 은 언제나 옳다(아마도)

---
+++ Python3/Level1/수박수박수박수박수박수?.py
@@ -0,0 +1,9 @@
+#https://programmers.co.kr/learn/courses/30/lessons/12922
+def solution(n):
+    return '수박'*(n//2)+'수' if n%2 else '수박'*(n//2)
+'''
+다른 사람의 풀이
+def water_melon(n):
+    return ("수박"*n)[:n]
+느낌 있네
+'''
